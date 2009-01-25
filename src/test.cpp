c3b1ba97fe52cac5ade65c1c9f4e0420c3438479
Samuel Henderson <Samuel.C.Henderson@gmail.com>
2009-01-25 03:47:55 +0000

    modified test code

--- src/test.cpp
+++ src/test.cpp
@@ -20,8 +20,8 @@ int main(void)
    install_keyboard(); 
 
    /* set a graphics mode sized 320x200 */
-   if (set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0) != 0) {
-      if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
+   if (set_gfx_mode(GFX_AUTODETECT, 1280, 1024, 0, 0) != 0) {
+      if (set_gfx_mode(GFX_SAFE, 1280, 1024, 0, 0) != 0) {
 	 set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
 	 allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
 	 return 1;
