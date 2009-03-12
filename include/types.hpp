fa4ff67276b25c8d8977c113fe0bcd220278b8f7
Brandon McCaig <bamccaig@gmail.com>
2009-03-12 23:55:49 +0000

    Forgot to add the types.hpp header file to the repository. Fixed.

---
+++ include/types.hpp
@@ -0,0 +1,29 @@
+
+#ifndef IQ_TYPES_HPP
+	#define IQ_TYPES_HPP
+
+	#include <allegro.h>
+	#include <boost/shared_ptr.hpp>
+	#include <map>
+	#include <semaphore.h>
+	#include <tinyxml.h>
+	#include <vector>
+
+namespace iq
+{
+	typedef unsigned int uint;
+
+	typedef std::map<std::string, std::string> string_map;
+	typedef std::vector<std::string> string_vector;
+
+	typedef boost::shared_ptr<BITMAP> BITMAP_ptr;
+	typedef boost::shared_ptr<BITMAP> BITMAP_ptr;
+	typedef boost::shared_ptr<TiXmlDocument> TiXmlDocument_ptr;
+	typedef boost::shared_ptr<sem_t> sem_t_ptr;
+	typedef boost::shared_ptr<string_map> string_map_ptr;
+	typedef boost::shared_ptr<string_vector> string_vector_ptr;
+	typedef boost::shared_ptr<uint> uint_ptr;
+}
+
+#endif
+
