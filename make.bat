ea036d732b5fbce385de9175f4beef1f2816f765
Brandon McCaig <bamccaig@gmail.com>
2009-03-21 20:54:32 +0000

    Added a check for the `make' command so if mingw32-make is not there it will try just make...

--- make.bat
+++ make.bat
@@ -1,11 +1,25 @@
 @echo off
 
+if [%MINGDIR%] == [] (
+    echo Please create the MINGDIR environment variable, pointing to your MinGW
+	echo installation.
+	echo.
+    exit /b -1
+)
+
 :: This is just a little helper batch file to allow you to build/clean the
 :: project with `make' and `make clean' like in Linux (after symlinking a
 :: Makefile, that is).
-if [%1] == [-f] (
-    mingw32-make %*
+if exist "%MINGDIR%\bin\mingw32-make.exe" (
+    if [%1] == [-f] (
+        "%MINGDIR%\bin\mingw32-make" %* WINDOWS=YES
+    ) else (
+        "%MINGDIR%\bin\mingw32-make" -f Makefile.bam %* WINDOWS=YES
+    )
 ) else (
-    mingw32-make -f Makefile.bam %*
+    if [%1] == [-f] (
+        "%MINGDIR%\bin\make" %* WINDOWS=YES
+    ) else (
+        "%MINGDIR%\bin\make" -f Makefile.bam %* WINDOWS=YES
+    )
 )
-
