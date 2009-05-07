23c65e17c77b2bb0df91fbef81c91be6ce65b525
Brandon McCaig <bamccaig@gmail.com>
2009-05-07 16:02:43 +0000

    This is my attempt to add a copy of the GNU GPL and copyright notices to the project. It's the first time I've done it so hopefully it is correct. ^_^

--- include/types.hpp
+++ include/types.hpp
@@ -1,3 +1,23 @@
+/*
+ * Intern's Quest is an adventure game.
+ * Copyright (C) 2009 Brandon McCaig, Samuel Henderson
+ *
+ * This file is part of Intern's Quest.
+ *
+ * Intern's Quest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Intern's Quest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Intern's Quest.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #ifndef IQ_TYPES_HPP
 	#define IQ_TYPES_HPP
